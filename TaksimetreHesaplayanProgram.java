151945b9ad0c922838a7ae21bb6a4183bbb200a2
umut <96980553+Umutcavuslar@users.noreply.github.com>
2022-05-28 02:26:54 +0300

    Update TaksimetreHesaplayanProgram.java

--- TaksimetreHesaplayanProgram.java
+++ TaksimetreHesaplayanProgram.java
@@ -5,7 +5,7 @@ Taksimetre KM başına 2.20 TL tutmaktadır.
 Minimum ödenecek tutar 20 TL'dir. 20 TL altında ki ücretlerde yine 20 TL alınacaktır.
 Taksimetre açılış ücreti 10 TL'dir.
 */
-public class taksi {
+public class test {
     public static void main(String[] args) {
         int km;
         double kmbasina=2.20, acilisucret=10, total;
